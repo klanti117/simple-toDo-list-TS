2abbcac7ba3835b8c5f072bc041c6e4a92a39091
klanti islam <klantyislam@yahoo.com>
2023-02-27 08:27:06 -0500

    added list

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,13 @@
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoInput");
 const form = document.querySelector("form");
+const list = document.getElementById("todoList"); /*asserting is not a good idea here,
+as the list initially will be empty */
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("submitted");
+    const newItem = input.value;
+    const newLI = document.createElement("li");
+    newLI.append(newItem);
+    list === null || list === void 0 ? void 0 : list.append(newLI); //optional chaining necessary
+    input.value = "";
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 const btn  = document.getElementById("btn")!
 const input  = document.getElementById("todoInput")! as HTMLInputElement
 const form  = document.querySelector("form")!
+const list = document.getElementById("todoList") /*asserting is not a good idea here, 
+as the list initially will be empty */
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault()
-    console.log("submitted")
+    const newItem = input.value
+    const newLI = document.createElement("li")
+    newLI.append(newItem)
+    list?.append(newLI)//optional chaining necessary
+    input.value=""
 })
 
