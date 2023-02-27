7c809e29b825df7bbdffb941239719dc6ef4e53b
klanti islam <klantyislam@yahoo.com>
2023-02-27 09:00:11 -0500

    added localStorage

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,24 @@
 "use strict";
-const todos = [];
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoInput");
 const form = document.querySelector("form");
 const list = document.getElementById("todoList"); /*asserting is not a good idea here,
 as the list initially will be empty */
+const readTodos = () => {
+    const todoJSON = localStorage.getItem("todos");
+    if (todoJSON === null)
+        return [];
+    return JSON.parse(todoJSON);
+};
+const renderTodo = (newTodo) => {
+    const newLI = document.createElement("li");
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    newLI.append(newTodo.text, checkBox);
+    list === null || list === void 0 ? void 0 : list.append(newLI); //optional chaining necessary
+};
+const todos = readTodos();
+todos.forEach(renderTodo);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const newTodo = {
@@ -13,12 +27,6 @@ form.addEventListener("submit", (e) => {
     };
     todos.push(newTodo);
     renderTodo(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
     input.value = "";
 });
-const renderTodo = (newTodo) => {
-    const newLI = document.createElement("li");
-    const checkList = document.createElement("input");
-    checkList.type = "checkbox";
-    newLI.append(newTodo.text, checkList);
-    list === null || list === void 0 ? void 0 : list.append(newLI); //optional chaining necessary
-};
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,29 @@ type Todo = {
     text: string, 
     isCompleted: boolean
 }
-const todos: Todo[]=[]
-const btn  = document.getElementById("btn")!
-const input  = document.getElementById("todoInput")! as HTMLInputElement
-const form  = document.querySelector("form")!
+const btn = document.getElementById("btn")!
+const input = document.getElementById("todoInput")! as HTMLInputElement
+const form = document.querySelector("form")!
 const list = document.getElementById("todoList") /*asserting is not a good idea here, 
 as the list initially will be empty */
+
+const readTodos = () => {
+    const todoJSON = localStorage.getItem("todos")
+    if (todoJSON === null) return []
+    return JSON.parse(todoJSON)
+}
+
+const renderTodo = (newTodo: Todo) => {
+    const newLI = document.createElement("li")
+    const checkBox = document.createElement("input")
+    checkBox.type = "checkbox"
+    newLI.append(newTodo.text, checkBox)
+    list?.append(newLI)//optional chaining necessary
+}
+
+const todos: Todo[] = readTodos()
+todos.forEach(renderTodo)
+
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault()
@@ -15,15 +32,11 @@ form.addEventListener("submit",(e)=>{
         text: input.value,
         isCompleted: false
     }
+    
     todos.push(newTodo)
     renderTodo(newTodo)
+    
+    localStorage.setItem("todos", JSON.stringify(todos))
     input.value=""
 })
 
-const renderTodo = (newTodo: Todo)=>{
-    const newLI = document.createElement("li")
-    const checkList = document.createElement("input")
-    checkList.type = "checkbox"
-    newLI.append(newTodo.text, checkList)
-    list?.append(newLI)//optional chaining necessary
-}
